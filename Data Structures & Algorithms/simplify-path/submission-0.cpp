c4573adcfa6eaeff4b85e602a79385448eaa4d82
Kashish Yadav <kashishyadav9559@gmail.com>
2026-07-14 18:12:28 +0530

    Add: simplify-path - submission-0

---
+++ Data Structures & Algorithms/simplify-path/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        string token="";
+        stringstream ss(path);
+        stack<string>st;
+        while(getline(ss,token,'/')){
+            if(token=="."||token=="") continue;
+            else if(token==".."){
+                if(!st.empty()) st.pop();
+            }
+            else st.push(token);
+
+        }
+        if(st.empty()) return "/";
+        string result;
+        while(!st.empty()){
+            result="/"+st.top()+result;
+            st.pop();
+        }
+        return result;
+    }
+};
